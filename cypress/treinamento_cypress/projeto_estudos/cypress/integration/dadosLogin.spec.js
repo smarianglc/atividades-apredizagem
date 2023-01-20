512f63595de65690d8bf70b2456b78a709f398a3
Maria Angelica <mariancademico@gmail.com>
2023-01-20 15:03:00 -0400

    estudando

---
+++ cypress/treinamento_cypress/projeto_estudos/cypress/integration/dadosLogin.spec.js
@@ -0,0 +1,11 @@
+context('LojaWeb20 Demo Produção', () => {
+    it('Fazer login ', () => {
+        //entender o fluxo manualmente 
+        //mapear os elementos da tela
+        //desenvolver os testes automatizados
+        //adcionar as asserções para validar os resultados dos testes
+        
+    });    
+
+    //
+});
