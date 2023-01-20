2e5a5837699f9b82ef0ecb7b9865aa142a78a893
Maria Angelica <mariancademico@gmail.com>
2023-01-20 09:28:07 -0400

    atualizando

--- cypress/primeiro_projeto/cypress/e2e/1-getting-started/testando_botoes.cy.js
+++ cypress/primeiro_projeto/cypress/e2e/1-getting-started/testando_botoes.cy.js
@@ -7,7 +7,13 @@ describe('fazer login', () =>{
 
     //segundo iniciar novo caso de teste
     it('abrir menu', () =>{
-       //cy.get(':nth-child(2) > .mt-10').should('have.text','titulete');
-        cy.get(':nth-child(2) > .mt-5 > .botao').click();
+        cy.get('.cp-topo-component .menu .v-btn').click(); //abrir o menu
+        cy.get('v-btn v-btn--contained theme--light v-size--default').click(); //clicar em "entrar"
+        
+        //INFORMAR DADOS DE LOGIN
+        cy.get('#input-113').click(); //clicar no campo "cpf"
+        cy.get('#input-113').type('02792595140'); //preencher o campo "cpf"
+        cy.get('#input-115').click(); //clicar no campo "senha"
+        cy.get('#input-115').type('123123123'); //informar "senha"
     })
 })
