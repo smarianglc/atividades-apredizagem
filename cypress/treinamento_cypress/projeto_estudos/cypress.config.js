9b003cb2b7e9784d79d02d6758c0496484150280
Maria Angelica <mariancademico@gmail.com>
2023-02-23 15:12:06 -0400

    reports-html

--- cypress/treinamento_cypress/projeto_estudos/cypress.config.js
+++ cypress/treinamento_cypress/projeto_estudos/cypress.config.js
@@ -2,8 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    "reporter": "mochawesome",
+    "reporterOptions":{
+        "reportDir": "cypress/reports/mochawesome-report",
+        "overwrite": true,
+        "html": true,
+        "json": false,
+        "timestamp": "YYYYMMDD_HHmmss"
     },
   },
 });
+
+
